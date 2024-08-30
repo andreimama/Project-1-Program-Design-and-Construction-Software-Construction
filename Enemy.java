db6b5ec00fd702f9a792bf6f5f118be831df9d36
andreimama <132239794+andreimama@users.noreply.github.com>
2024-08-31 03:12:55 +1200

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -11,40 +11,49 @@ package com.mycompany.justanothertuesday;
 
     public abstract class Enemy
 {
+  //encapsulation of private fields
   private String mobName = "";
   private int healthPoints;
   private int damage = 30;
 
+  //constructor that intialise name and hp
   public Enemy(String name, int healthPoints )
   {
     this.mobName = name;
     this.healthPoints = healthPoints;
     
   }  
-  //set name of 
+  //set names of enemy
   public void setName(String newName) {
         this.mobName = newName;
     }
+  //get name of enemy
   public String getName() {
       return this.mobName;
   }
   
+  //method sets health of enemy
   public void setHealth(int hp) {
         this.healthPoints = hp;
     }
+  
+  //method to gets health of eney
   public int getHealth() {
       return this.healthPoints;
   }
   
+  
+  //sets damage of Enemys
   public void setDamage(int damage) {
       this.damage = damage;
   }
-  
+  //method to get damage of enemy
   public int getDamage() {
       return this.damage;
   }
   
 //abstract methods that are implemented by SubBoss class
+  //proof of polymorphism
 public abstract int dealsDamage();
 
 public abstract int takesDamage(int damage);
