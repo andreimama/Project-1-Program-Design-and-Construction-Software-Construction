f151ece411b452db54db9e2c819a723b8d10e14f
andreimama <132239794+andreimama@users.noreply.github.com>
2024-08-31 02:08:11 +1200

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -11,9 +11,9 @@ package com.mycompany.justanothertuesday;
 
     public abstract class Enemy
 {
-  public String mobName = "";
-  public int healthPoints;
-  public int damage;
+  private String mobName = "";
+  private int healthPoints;
+  private int damage = 30;
 
   public Enemy(String name, int healthPoints )
   {
@@ -21,12 +21,30 @@ package com.mycompany.justanothertuesday;
     this.healthPoints = healthPoints;
     
   }  
-  
+  //set name of 
   public void setName(String newName) {
         this.mobName = newName;
     }
+  public String getName() {
+      return this.mobName;
+  }
   
-
+  public void setHealth(int hp) {
+        this.healthPoints = hp;
+    }
+  public int getHealth() {
+      return this.healthPoints;
+  }
+  
+  public void setDamage(int damage) {
+      this.damage = damage;
+  }
+  
+  public int getDamage() {
+      return this.damage;
+  }
+  
+//abstract methods that are implemented by SubBoss class
 public abstract int dealsDamage();
 
 public abstract int takesDamage(int damage);
