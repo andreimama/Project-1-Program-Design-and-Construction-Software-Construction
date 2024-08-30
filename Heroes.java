a34ae938ca21a37e93ce54c6faf2659fe5729fd9
andreimama <132239794+andreimama@users.noreply.github.com>
2024-08-31 11:40:55 +1200

    Update Heroes.java

--- Heroes.java
+++ Heroes.java
@@ -4,9 +4,12 @@ package justanothertuesday;
  *
  * @author franc
  */
+//interface class
 public interface Heroes {
     void takesDamage(int amount);
     void powerUp(int amount);
     void addHealth(Boolean thing, int amount);
+    void setHealth(int health);
+    void setDamage(int damage);
     
 }
