27b33c7d2eb1ded77efaf3bba711816cdbce208d
andreimama <132239794+andreimama@users.noreply.github.com>
2024-08-31 14:48:05 +1200

    Update Stealth.java

--- Stealth.java
+++ Stealth.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.justanothertuesday;
 
 import java.util.Random;
@@ -7,16 +10,22 @@ import java.util.Random;
  *
  * @author franc
  */
+
+//implements the Heroes interface and inherits the methods. - inheritance
 public class Stealth implements Heroes {
-    private final String name ;
+    //private fields with public getters and setters.
+    private String name ;
     private int health;
     private int dealDamage;
     
+    //Constructor initalizing the fields.
     public Stealth(){
         this.name = "Sir Sneaksalot";
         this.health = 100;
         this.dealDamage = 30;
     }
+    
+    //Polymorphism - Overriding methods from heroes interface.
     @Override
     public void takesDamage(int amount){
         this.health = this.getHealth() - amount;
@@ -25,6 +34,33 @@ public class Stealth implements Heroes {
     public void powerUp(int amount){
         this.dealDamage = this.getDealDamage() + amount;
     }
+    
+    @Override
+    public void addHealth(Boolean thing, int amount){
+        if(thing){
+            this.health += amount;
+        }
+    }
+    @Override
+    public void setDealDamage(int damage) {
+        this.dealDamage = damage;
+    }
+    
+    @Override
+    public int getDealDamage() {
+        return dealDamage;
+    }
+    
+    @Override
+    public void setHealth(int Health) {
+        this.health = Health;
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+    //Calculates if attack will be critical or not
     public int criticalHit(){
         Random rand = new Random();
         int chance = rand.nextInt(20);
@@ -36,30 +72,11 @@ public class Stealth implements Heroes {
         return this.dealDamage;
         
     } 
-    public void addHealth(Boolean thing, int amount){
-        if(thing){
-            this.health += amount;
-        }
-    }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @return the health
-     */
+    //gets and returns health
     public int getHealth() {
         return health;
     }
 
-    /**
-     * @return the dealDamage
-     */
-    public int getDealDamage() {
-        return dealDamage;
-    }
-}
+    
+} 
